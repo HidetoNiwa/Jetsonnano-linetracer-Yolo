734466dc467e93f58b17e6c37cdb63f6e639a2ad
HidetoNiwa <tototohide@gmail.com>
2021-09-18 16:04:44 +0900

    add motor fanction

--- mbedProgram/main.cpp
+++ mbedProgram/main.cpp
@@ -4,30 +4,86 @@
  */
 
 #include "mbed.h"
+#include <cstdio>
+#include <string>
 
-PwmOut a1(A1);
+PwmOut a[]{
+ PwmOut (A1),
+ PwmOut (A2)
+};
 
+PwmOut b[]{
+ PwmOut (A3),
+ PwmOut (A5)
+};
+
+
+static UnbufferedSerial pc(USBTX, USBRX);
 
 void setPower(float power,int motorNum){
-    if(power>0){
-        a1=1.f;
-    }else{
-        a1=power;
+    if(power>1){
+        power=1.f;
+    }else if(power<-1){
+        power=-1;
+    }
+
+
+    if (motorNum==1){
+        if(power>0){
+            a[0]=1.f;
+            a[1]=1.f-power;
+        }else{
+            a[1]=1.f;
+            a[0]=1.f-fabs(power);
+        }
+    }else if(motorNum==2){
+        if(power>0){
+            b[0]=1.f;
+            b[1]=1.f-power;
+        }else{
+            b[1]=1.f;
+            b[0]=1.f-fabs(power);
+        }
+
     }
 }
 
 int main()
 {
-    printf("Hello, Mbed!\n");
+    //printf("Hello, Mbed!\n");
+
+    pc.baud(9600);
+    pc.format(
+        /* bits */ 8,
+        /* parity */ SerialBase::None,
+        /* stop bit */ 1
+    );
+
+    std::string str;
+
+    str="hellow?\r\n";
 
     while(1){
-	    a1=0.f;
-	    printf("output:0\t");
+        pc.write(str.c_str(),str.size());
+	    setPower(0.9,1);
+        setPower(0.9,2);
+	    //printf("output:0\t");
 	    ThisThread::sleep_for(1000);
 
-	    a1=0.8f;
-	    printf("output:0.8\n");
+        setPower(0,1);
+        setPower(0,2);
+        //printf("output:0.8\n");
+        ThisThread::sleep_for(1000);
+
+	    setPower(-0.9,1);
+        setPower(-0.9,2);
+	    //printf("output:0.8\n");
 	    ThisThread::sleep_for(1000);
+
+        setPower(0,1);
+        setPower(0,2);
+        //printf("output:0.8\n");
+        ThisThread::sleep_for(1000);
     }
     return 0;
 }
